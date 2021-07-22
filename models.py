7385a37004eb1113141111fe75a737d3f8560dca
zmunisi <zmunisi.ext@gmail.com>
2021-07-22 09:10:35 +0200

    Return variable changes

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 import json
 
-database_path = os.environ['DATABASE_URL']
+database_path = os.environ['SQLALCHEMY_DATABASE_URI']
 
 db = SQLAlchemy()
 
@@ -12,7 +12,7 @@ setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
 def setup_db(app, database_path=database_path):
-    app.config["DATABASE_URL"] = database_path
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
